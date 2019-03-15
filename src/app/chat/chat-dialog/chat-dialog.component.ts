27104899e210fb8c81c88a83dc377bd10a7b4e66
sarahgawde <43248486+sarahgawde@users.noreply.github.com>
2019-03-16 05:14:26 +0530

    Add files via upload

---
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../../chat.service';
+import { Observable } from 'rxjs';
+import { scan } from 'rxjs/operators';
+//import 'rxjs/add/operator/scan';
+
+
+@Component({
+  selector: 'chat-dialog',
+  templateUrl: './chat-dialog.component.html',
+  styleUrls: ['./chat-dialog.component.css']
+})
+export class ChatDialogComponent implements OnInit {
+  messages: Observable<Message[]>;
+  formValue: string;
+
+  constructor(public chat: ChatService) { }
+
+  ngOnInit() {
+    this.messages = this.chat.conversation.asObservable().pipe(scan((acc, val) => acc.concat(val)));
+  }
+  sendMessage() {
+    this.chat.converse(this.formValue);
+    this.formValue = '';
+  }
+
+
+}
